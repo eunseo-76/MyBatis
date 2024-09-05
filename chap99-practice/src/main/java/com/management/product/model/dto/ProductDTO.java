0ab73c42357f4d8c0f1c183bf60505e5510b3a44
eunseo-76 <eunseolee19@gmail.com>
2024-09-05 17:16:59 +0900

    backup : 실습99 풀기

---
+++ chap99-practice/src/main/java/com/management/product/model/dto/ProductDTO.java
@@ -0,0 +1,118 @@
+package com.management.product.model.dto;
+
+public class ProductDTO {
+
+    // * 주석을 지우고 제품 정보를 담는 객체를 만들 java class를 작성하세요.
+    // * 모든 필드는 String 타입으로 작성하시고, 'PRODUCT_INFO' 테이블과 매핑 가능해야 합니다.
+    private String productCode;
+    private String productName;
+    private String originCost;
+    private String releaseDate;
+    private String discountRate;
+    private String salesQuantity;
+    private String stockQuantity;
+    private String categoryCode;
+    private String productStatus;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(String productCode, String productName, String originCost, String releaseDate, String discountRate, String salesQuantity, String stockQuantity, String categoryCode, String productStatus) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.originCost = originCost;
+        this.releaseDate = releaseDate;
+        this.discountRate = discountRate;
+        this.salesQuantity = salesQuantity;
+        this.stockQuantity = stockQuantity;
+        this.categoryCode = categoryCode;
+        this.productStatus = productStatus;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getOriginCost() {
+        return originCost;
+    }
+
+    public void setOriginCost(String originCost) {
+        this.originCost = originCost;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(String discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    public String getSalesQuantity() {
+        return salesQuantity;
+    }
+
+    public void setSalesQuantity(String salesQuantity) {
+        this.salesQuantity = salesQuantity;
+    }
+
+    public String getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(String stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(String categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(String productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDTO{" +
+                "productCode='" + productCode + '\'' +
+                ", productName='" + productName + '\'' +
+                ", originCost='" + originCost + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", discountRate='" + discountRate + '\'' +
+                ", salesQuantity='" + salesQuantity + '\'' +
+                ", stockQuantity='" + stockQuantity + '\'' +
+                ", categoryCode='" + categoryCode + '\'' +
+                ", productStatus='" + productStatus + '\'' +
+                '}';
+    }
+}
---
+++ chap99-practice/src/main/java/com/management/product/model/service/ProductService.java
@@ -0,0 +1,66 @@
+package com.management.product.model.service;
+
+import com.common.SearchCondition;
+import com.management.product.model.dao.ProductDAO;
+import com.management.product.model.dto.ProductDTO;
+import org.apache.ibatis.jdbc.SelectBuilder;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.common.Template.getSqlSession;
+
+public class ProductService {
+
+    // * 주석을 지우고 Service 역할에 해당하는 메소드를 작성하세요.
+
+    // 1. 자주 사용할 DAO 객체를 선언하세요.
+
+    public List<ProductDTO> selectAllProductList() {
+        SqlSession sqlSession = getSqlSession();
+        ProductDAO productDAO = sqlSession.getMapper(ProductDAO.class);
+
+        List<ProductDTO> productList = productDAO.selectAllProductList();
+        sqlSession.close();
+
+        return productList;
+
+
+        // 2. 전체 제품 목록을 조회하는 로직을 작성하세요.
+        // 　　아래 작성된 return null은 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
+
+    }
+
+    public List<ProductDTO> selectProductByCondition(SearchCondition searchCondition) {
+
+        // 3. 조건에 따른 제품 목록을 조회하는 로직을 작성하세요.
+        // 　　아래 작성된 return null은 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
+        return null;
+
+    }
+
+    public boolean registNewProduct(ProductDTO product) {
+
+        // 4. 제품 정보를 등록하는 로직을 작성하세요.
+        // 　　아래 작성된 return false 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
+        return false;
+
+    }
+
+    public boolean modifyProductInfo(ProductDTO product) {
+
+        // 5. 제품 정보를 수정하는 로직을 작성하세요.
+        // 　　아래 작성된 return false 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
+        return false;
+
+    }
+
+    public boolean deleteProduct(Map<String, String> parameter) {
+
+        // 6. 제품 정보를 삭제하는 로직을 작성하세요.
+        // 　　아래 작성된 return false 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
+        return false;
+
+    }
+}
